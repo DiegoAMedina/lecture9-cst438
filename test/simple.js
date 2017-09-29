baad20f56f6b1167d13a6f91566124e9aa82cea9
Diego Medina <dimedina@csumb.edu>
2017-09-29 02:23:34 +0000

    fixed the expect test at the simple.js

--- test/simple.js
+++ test/simple.js
@@ -6,7 +6,7 @@ var add = require("../simple-function").add;
 describe("Add function", function() {
     it("should return the sum of 2 numbers", function(done) {
        expect(add(2,4)).to.equal(6); 
-       expect(add(2,4)).to.equal(3); 
+       expect(add(2,1)).to.equal(3); 
        done(); 
     }); 
 
